c1d4739bd8814a1ccf74fe63d8f22ac16763e855
VulpoiuPetru <147253039+VulpoiuPetru@users.noreply.github.com>
2025-12-10 17:49:07 +0200

    Moving Objects

--- DomainModel/Copy.cs
+++ DomainModel/Copy.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataMapper
 {
-    public class Class1
+    internal class Copy
     {
     }
 }
--- DomainModel/Loan.cs
+++ DomainModel/Loan.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DomainModel
+namespace DataMapper
 {
-    public class Class1
+    internal class Loan
     {
     }
 }
