a9ac2a9324722a35a826901442322cc3025a6624
VulpoiuPetru <147253039+VulpoiuPetru@users.noreply.github.com>
2026-01-08 13:31:10 +0200

    Add changes for StartUp

--- DataMapper/RepoServiceCollectionMethod/RepoServiceCollection.cs
+++ DataMapper/RepoServiceCollectionMethod/RepoServiceCollection.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataMapper.RepoInterfaces;
+using DataMapper.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,22 @@ using System.Threading.Tasks;
 
 namespace DataMapper.RepoServiceCollectionMethod
 {
-    internal class RepoServiceCollection
+    public static class RepoServiceCollection
     {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
+            services.AddScoped<IBorrowedBooksRepository, BorrowedBooksRepository>();
+            services.AddScoped<ICopyRepository, CopyRepository>();
+            services.AddScoped<IDomainRepository, DomainRepository>();
+            services.AddScoped<IEditionRepository, EditionRepository>();
+            services.AddScoped<IExtensionRepository, ExtensionRepository>();
+            services.AddScoped<IReaderRepository, ReaderRepository>();
+
+            services.AddMemoryCache();
+            return services;
+        }
     }
 }
--- MyConsoleApplication/OnStart/OnStart.cs
+++ MyConsoleApplication/OnStart/OnStart.cs
@@ -1,12 +1,86 @@
-﻿using System;
+﻿using DataMapper;
+using DataMapper.RepoServiceCollectionMethod;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
+using System;
+using System.Configuration;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyConsoleApplication.OnStart
 {
-    internal class OnStart
+    public class OnStart
     {
+        public static IServiceProvider ConfigureServices()
+        {
+            // Configure Serilog
+            ConfigureLogging();
+
+            var services = new ServiceCollection();
+
+            // Add logging
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddSerilog(dispose: true);
+            });
+
+            // Add Infrastructure services
+            services.AddInfrastructureServices();
+
+            // Add ServiceLayer services
+            services.AddServiceLayerServices();
+
+            // Add DbContext
+            services.AddScoped<LibraryContext>();
+
+            return services.BuildServiceProvider();
+        }
+        private static void ConfigureLogging()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["LibraryDBConnectionString"]?.ConnectionString;
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("LibraryDBConnectionString is not configured in app.config");
+            }
+
+            var sqlSinkOptions = new MSSqlServerSinkOptions
+            {
+                TableName = "ApplicationLogs",
+                AutoCreateSqlTable = true,
+                BatchPostingLimit = 50,
+                BatchPeriod = TimeSpan.FromSeconds(2)
+            };
+
+            var columnOptions = new ColumnOptions
+            {
+                AdditionalColumns = new Collection<SqlColumn>
+                {
+                    new SqlColumn { ColumnName = "MachineName", DataType = SqlDbType.NVarChar, DataLength = 50 },
+                    new SqlColumn { ColumnName = "SourceContext", DataType = SqlDbType.NVarChar, DataLength = 128 }
+                }
+            };
+
+            columnOptions.Store.Remove(StandardColumn.Properties);
+            columnOptions.Store.Remove(StandardColumn.Id);
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console(
+                    restrictedToMinimumLevel: LogEventLevel.Information,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .WriteTo.MSSqlServer(
+                    connectionString: connectionString,
+                    sinkOptions: sqlSinkOptions,
+                    restrictedToMinimumLevel: LogEventLevel.Warning,
+                    columnOptions: columnOptions)
+                .CreateLogger();
+        }
     }
 }
